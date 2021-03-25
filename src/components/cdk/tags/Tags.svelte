ee2da1f7fd6aded1bc50ce8f00e8a40d649ba1e3
Anthony Le Goas <anthony.legoas@gmail.com>
2021-03-25 17:57:17 +0100

    ✨ add <Tags> & <Tag> components

---
+++ src/components/cdk/tags/Tags.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let tags;
+    import Tag from './tag/Tag.svelte'
+  </script>
+  
+  <style>
+    ul {
+      list-style: none;
+      display: flex;
+      padding: 0;
+    }
+  
+    @media (max-width: 600px) {
+      ul {
+        justify-content: center;
+      }
+    }
+  </style>
+  
+  <ul>
+    {#each tags as tag}
+      <li>
+        <Tag {tag}/>
+      </li>
+    {/each}
+  </ul>
+  
---
+++ src/components/cdk/tags/tag/Tag.svelte
@@ -0,0 +1,27 @@
+
+<script>
+    export let tag;
+</script>
+
+<style>
+    .tag {
+        color: var(--ssd-blue);
+    }
+
+    b {
+        font-size: var(--text-size-s);
+    }
+
+    @media (max-width: 600px) {
+        ul {
+        justify-content: center;
+        }
+
+        b {
+        text-align: center;
+        font-size: var(--text-size-xs);
+        }
+    }
+</style>
+
+<b class="tag">#{tag}&nbsp;</b>
