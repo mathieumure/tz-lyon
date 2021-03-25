4fa257c131a684a60aa55954abc943c3e759feac
Anthony Le Goas <anthony.legoas@gmail.com>
2021-03-25 16:51:41 +0100

    ✨ add Header component

---
+++ src/components/layout/header/Header.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<style>
+    header {
+        height: 100vh;
+        background: center url('/images/full-vert-billy.jpg');
+        color: white;
+    }
+</style>
+
+  
+<header>
+    <h1>Technozaure world 2021</h1>
+</header>
+  
