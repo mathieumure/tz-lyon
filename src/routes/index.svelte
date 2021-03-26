baf1c403e74049c0ae2eef1e9848f50ad1c9d741
Anthony Le Goas <anthony.legoas@gmail.com>
2021-03-26 10:34:10 +0100

    ✨ add <Talks> to home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,50 +1,21 @@
+<script context="module">
+	export async function preload(page, session) {
+		const res = await this.fetch(`_talks.json`);
+		const talks = await res.json();
+		return { talks };
+	}
+</script>
+
 <script>
-	import successkid from 'images/successkid.jpg';
+	import Talks from '../components/talks/Talks.svelte'
+	export let talks;
 </script>
 
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Technozaure World 2021 - Zenika</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<Talks {talks}/>
