6e4c7d5d05d69f92a55ed2ff41984dd96e0dfc78
Anthony Le Goas <anthony.legoas@gmail.com>
2021-03-26 10:31:58 +0100

    ✨ add <ImgRounded> component

---
+++ src/components/cdk/img/ImgRounded.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let alt, src;
+  
+    const defaultImage = "/images/speakers/blob-violet.png";
+  </script>
+  
+  <style>
+    img {
+      margin: 0 0.75rem;
+      width: 128px;
+      border-radius: 50%;
+    }
+  
+    @media (max-width: 600px) {
+      img {
+        width: 100px;
+      }
+    }
+  </style>
+  
+  <img {alt} src={src || defaultImage} />
+  
