8acad082242de8f2d78977eef9aff3dbd0471b52
Anthony Le Goas <anthony.legoas@gmail.com>
2021-03-29 18:16:05 +0200

    💄 update main layout style

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,22 @@
-<script>
-	import Header from '../components/layout/header/Header.svelte';
-</script>
-
 <style>
-  main {
-    position: relative;
-    background-color: black;
-    margin: 0 auto;
-    box-sizing: border-box;
-    padding-bottom: 5em;
-  }
+main {
+  position: relative;
+  background-color: var(--z-black);
+  margin: 0 auto;
+  box-sizing: border-box;
+  padding: 5em 0;
+}
 
-  @media (max-width: 1200px) {
-    main {
-      padding-bottom: 2em;
-    }
+@media (max-width: 1200px) {
+  main {
+    padding-bottom: 2em;
   }
+}
 </style>
+
+<script>
+import Header from "../components/layout/header/Header.svelte";
+</script>
 
 <Header />
 <main>
