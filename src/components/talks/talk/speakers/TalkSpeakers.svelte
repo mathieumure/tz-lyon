96b13d721f0360dfc778f1f44b4252963623c278
Anthony Le Goas <anthony.legoas@gmail.com>
2021-04-04 13:04:19 +0200

    ✨ add <TalkSpeakers> component

---
+++ src/components/talks/talk/speakers/TalkSpeakers.svelte
@@ -0,0 +1,27 @@
+<style>
+.speakers {
+  position: relative;
+  list-style: none;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 100%;
+}
+</style>
+
+<script>
+import Speaker from "../../../cdk/speaker/Speaker.svelte";
+
+export let speakers;
+</script>
+
+<ul class="speakers">
+  {#each speakers as speaker}
+    <li>
+      <Speaker id={speaker} />
+    </li>
+  {/each}
+</ul>
