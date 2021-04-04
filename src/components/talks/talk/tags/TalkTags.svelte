fe1cfb32fcb4d76124397c0dbb65df50ca31da2e
Anthony Le Goas <anthony.legoas@gmail.com>
2021-04-04 13:04:34 +0200

    ✨ add <TalkTags> component

---
+++ src/components/talks/talk/tags/TalkTags.svelte
@@ -0,0 +1,20 @@
+<style>
+</style>
+
+<script>
+import Tags from "../../../cdk/tags/Tags.svelte";
+import { eventStore } from "../../../../stores";
+
+export let categoryId, formatId, language, level;
+
+const { categories, formats } = eventStore;
+
+$: tags = [
+  language === "French" ? "🇫🇷 FR" : "🇬🇧 EN",
+  $categories.find((c) => c.id === categoryId).name,
+  $formats.find((f) => f.id === formatId).name,
+  level,
+];
+</script>
+
+<Tags {tags} />
