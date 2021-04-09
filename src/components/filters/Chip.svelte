35eed03735a08130cccac001d966f343dcb34e4b
murgatt <theomurgat@hotmail.fr>
2021-04-10 01:29:30 +0200

    fix chip styles

--- src/components/filters/Chip.svelte
+++ src/components/filters/Chip.svelte
@@ -13,22 +13,48 @@
         align-items: center;
         height: 32px;
         border-radius: 16px;
-        border: 1px solid rgba(255, 255, 255, 0.23);
         padding: 0 16px;
         cursor: pointer;
         color: white;
-        transition: background-color 300ms ease-in-out;
+        text-transform: uppercase;
+        position: relative;
+        overflow: hidden;
+        border: 0;
+        background: none;
+        outline: none;
     }
 
-    .chip:hover {
-        background-color: rgba(255, 255, 255, 0.08);
+    .chip span {
+        position: relative;
+        z-index: 2;
     }
 
-    .chip--selected, .chip--selected:hover {
-        background-color: #616161;
+    .chip:before, .chip:after {
+        content: '';
+        position: absolute;
+        left: 0;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        z-index: 1;
+    }
+
+    .chip:before {
+        background: var(--z-grey);
+    }
+
+    .chip:after {
+        opacity: 0;
+        background: #ee2238;
+        background: linear-gradient(66deg, #ee2238 0%, #ee2238 50%, #4610df 100%);
+        transition: opacity 300ms ease-in-out;
+    }
+
+    .chip:hover:after, .chip--selected:after, .chip:focus:after {
+        opacity: 1;
     }
 </style>
 
-<div class="chip {selected ? 'chip--selected' : ''}" role="button" on:click={handleClick}>
-    {label}
-</div>
+<button class="chip {selected ? 'chip--selected' : ''}" role="button" on:click={handleClick}>
+    <span>{label}</span>
+</button>
