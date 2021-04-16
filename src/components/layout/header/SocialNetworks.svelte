80f3dba59b0c370009f3f09e7e6901486ee799ab
Anthony Le Goas <anthony.legoas@gmail.com>
2021-04-16 10:11:36 +0200

    ⚡️ explicit img width and height on <SocialNetworks>

--- src/components/layout/header/SocialNetworks.svelte
+++ src/components/layout/header/SocialNetworks.svelte
@@ -1,36 +1,59 @@
+<style>
+ul {
+  display: flex;
+}
+
+li {
+  margin-right: 16px;
+}
+
+li:last-child {
+  margin-right: 0;
+}
+
+a {
+  display: inline-block;
+}
+
+img {
+  height: 1.5rem;
+  width: 1.5rem;
+}
+</style>
+
 <script>
-  const networks = [
-    {
-      name: "Twitter",
-      icon: "twitter.svg",
-      link: "https://twitter.com/ZenikaIT",
-    },
-    {
-      name: "YouTube",
-      icon: "youtube.svg",
-      link: "https://www.youtube.com/user/ZenikaITTv",
-    },
-    {
-      name: "Facebook",
-      icon: "facebook.svg",
-      link: "https://www.facebook.com/ZenikaIT/",
-    },
-    {
-      name: "LinkedIn",
-      icon: "linkedin.svg",
-      link: "https://www.linkedin.com/company/zenika/",
-    },
-    {
-      name: "Instagram",
-      icon: "instagram.svg",
-      link: "https://www.instagram.com/zenika_it/",
-    },
-    {
-      name: "Twitch",
-      icon: "twitch.svg",
-      link: "https://www.twitch.tv/zenikait",
-    },
-  ];
+const networks = [
+  {
+    name: "Twitter",
+    icon: "twitter.svg",
+    link: "https://twitter.com/ZenikaIT",
+  },
+  {
+    name: "YouTube",
+    icon: "youtube.svg",
+    link: "https://www.youtube.com/user/ZenikaITTv",
+  },
+  {
+    name: "Facebook",
+    icon: "facebook.svg",
+    link: "https://www.facebook.com/ZenikaIT/",
+  },
+  {
+    name: "LinkedIn",
+    icon: "linkedin.svg",
+    link: "https://www.linkedin.com/company/zenika/",
+  },
+  {
+    name: "Instagram",
+    icon: "instagram.svg",
+    link: "https://www.instagram.com/zenika_it/",
+  },
+  {
+    name: "Twitch",
+    icon: "twitch.svg",
+    link: "https://www.twitch.tv/zenikait",
+  },
+];
 </script>
 
 <ul class={$$props.class}>
@@ -42,25 +65,3 @@
     </li>
   {/each}
 </ul>
-
-<style>
-  ul {
-    display: flex;
-  }
-
-  li {
-    margin-right: 16px;
-  }
-
-  li:last-child {
-    margin-right: 0;
-  }
-
-  a {
-    display: inline-block;
-  }
-
-  img {
-    height: 1.5rem;
-  }
-</style>
