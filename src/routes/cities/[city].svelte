c9e7a54a8f3b3a9c4accd8cabdf6dcaa21bbef7c
Anthony Le Goas <anthony.legoas@gmail.com>
2021-04-20 19:28:31 +0200

    ✨ add city page

---
+++ src/routes/cities/[city].svelte
@@ -0,0 +1,31 @@
+<script context="module">
+import { eventStore } from "../../stores";
+
+export async function preload(page, session) {
+  const res = await this.fetch(`event.json`);
+  const event = await res.json();
+  const { city } = page.params;
+  event.talks = event.talks.filter(
+    (talk) => talk.trackName.toUpperCase() === city.toUpperCase()
+  );
+
+  return { city: `${city.charAt(0).toUpperCase()}${city.slice(1)}`, event };
+}
+</script>
+
+<script>
+import Talks from "../../components/talks/Talks.svelte";
+import Filters from "../../components/filters/Filters.svelte";
+export let city, event;
+
+const { setEvent } = eventStore;
+setEvent(event);
+</script>
+
+<svelte:head>
+  <title>Technozaure World 2021 - Zenika - {city}</title>
+</svelte:head>
+
+<h1>Le programme à {city}</h1>
+<Filters />
+<Talks />
