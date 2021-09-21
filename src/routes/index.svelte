fc4ec79d418764bfba17fd0dab25dbab1ef0248d
nlepage <19571875+nlepage@users.noreply.github.com>
2021-09-21 14:10:44 +0200

    Sort par startTime

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,6 +5,18 @@ export async function preload(page, session) {
   const res = await this.fetch(`event.json`);
   const event = await res.json();
 
+  event.talks.sort((t1, t2) => {
+      const [t1h1, t1h2] = t1.startTime.split("h");
+      const [t2h1, t2h2] = t2.startTime.split("h");
+
+      if (t2h1 < t1h1) return 1;
+      else if (t2h1 > t1h1) return -1;
+      else if (t2h2 < t1h2) return 1;
+      else if (t2h2 > t1h2) return -1;
+
+      return 0;
+    })
+
   return { event };
 }
 </script>
